b59f11c7d098199129101827c690fd9b5d03f96f
Martin Skalický <skalicky@modernalt.eu>
2024-06-06 21:25:05 +0200

    Add files via upload

---
+++ head.php
@@ -0,0 +1,11 @@
+<meta name="description" content="Discover the exquisite PressLine Color Palette, offering a spectrum of vibrant hues to elevate your designs. Explore our diverse range of colors and unleash your creativity today.">
+<meta name="keywords" content="PressLine, Color Palette, Design, Creativity, Vibrant Hues">
+<meta name="author" content="Martin Skalicky">
+<meta property="og:title" content="PressLine Color Palette">
+<meta property="og:description" content="Discover the exquisite PressLine Color Palette, offering a spectrum of vibrant hues to elevate your designs. Explore our diverse range of colors and unleash your creativity today.">
+<meta property="og:image" content="https://pressline.app/media/img/logo-full.png">
+<meta property="og:url" content="https://pressline.app/media/img/logo-full.png">
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:title" content="PressLine Color Palette">
+<meta name="twitter:description" content="Discover the exquisite PressLine Color Palette, offering a spectrum of vibrant hues to elevate your designs. Explore our diverse range of colors and unleash your creativity today.">
+<meta name="twitter:image" content="https://pressline.app/media/img/logo-full.png">
